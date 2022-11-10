d0d8d6d4702b6447a544d973bfa8be876941203c
maria valentina <mvchaparro6@misena.edu.co>
2022-11-10 08:45:35 -0500

    video29

---
+++ video29.php
@@ -0,0 +1,19 @@
+<?php
+ini_set ("display_errors", "On");
+
+ini_set ("max_execution_time", '5');
+
+ini_set('include_path',ini_get('include_path').':/your_include_dir:');
+
+
+
+//Get
+//Obtiene el valor de la opción que se va a configurar
+
+echo 'display_errors = ' . ini_get('display_errors') . "\n";
+echo 'register_globals = ' . ini_get('register_globals') . "\n";
+echo 'post_max_size = ' . ini_get('post_max_size') . "\n";
+echo 'post_max_size+1 = ' . (ini_get('post_max_size')+1) . "\n";
+echo 'post_max_size en bytes = ' . return_bytes(ini_get('post_max_size'));
+
+
