7f35d5cd8824011fb24397d4219d25b76c5bac23
Valentina <mvchaparro6@misena.edu.co>
2022-11-10 07:57:05 -0500

    Arrays

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'raveren/kint' => array(
+            'pretty_version' => 'v1.2.10',
+            'version' => '1.2.10.0',
+            'reference' => '16b6ed64d7c3dfd215f44497f98fb1d49cdbdd43',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../raveren/kint',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
