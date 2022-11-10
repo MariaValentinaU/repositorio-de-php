d80e7f212d139094ef41dac7df329b757cede82a
maria valentina <mvchaparro6@misena.edu.co>
2022-11-10 10:13:25 -0500

    actualizacion

--- video29.php
+++ video29.php
@@ -13,7 +13,5 @@ ini_set('include_path',ini_get('include_path').':/your_include_dir:');
 echo 'display_errors = ' . ini_get('display_errors') . "\n";
 echo 'register_globals = ' . ini_get('register_globals') . "\n";
 echo 'post_max_size = ' . ini_get('post_max_size') . "\n";
-echo 'post_max_size+1 = ' . (ini_get('post_max_size')+1) . "\n";
-echo 'post_max_size en bytes = ' . return_bytes(ini_get('post_max_size'));
-
+echo 'post_max_size+1 = ' . (ini_get('post_max_size')+3) . "\n";
 
