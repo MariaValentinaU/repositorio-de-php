74b317d946265aec04652cdbc900f32c539a420b
Maria Valentina <mvchaparro6@misena.edu.co>
2022-11-16 14:53:18 -0500

    nuevo commit

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejercicios</title>
-</head>
-<body>
+
 
 <?php
+
 include 'vendor/autoload.php';
 
 //ARRAY NORMAL
@@ -119,4 +112,14 @@ $a=array("a"=>"Rojo","b"=>"Verde","c"=>"Azul");
 echo array_search("Rojo",$a);
 echo '<br>';
 
-?>
+//array filter filtra los valores que deseemos
+
+function test_odd($var)
+{
+    return($var & 1);
+}
+
+$a1=array(1,3,2,3,4,6);
+d(array_filter($a1,"test_odd"));
+echo '<br>';
+
